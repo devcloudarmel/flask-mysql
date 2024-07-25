87becebb51bd6e12a8b67e53f1eb2297249beec3
DevCloudArmel <devcloudarmel@gmail.com>
2024-07-25 20:42:56 +0300

    Finally done

--- app/connection.py
+++ app/connection.py
@@ -1,7 +1,7 @@
 config = {
-    'user': 'root',
-    'password': '',
-    'host': 'localhost',
+    'user': 'admin',
+    'password': '123456789',
+    'host': 'mybooks.cjeag286c22o.us-east-1.rds.amazonaws.com',
     'port': 3306,
     'database': 'mybooks'
 }
